d5dfb41d4768c21da18314eaf676daff2ebc6fad
JeremiasTH <dylanescalada.90@gmail.com>
2026-08-01 19:31:02 -0300

    Delete script.js

--- script.js
+++
@@ -1,1 +0,0 @@
-setTimeout(()=>{document.getElementById('msg').style.opacity=1;document.getElementById('msg').textContent='Al igual que un girasol busca la luz, espero que siempre encuentres motivos para sonreír. 🌻 Hecho con cariño para Wilmelly.';},5000);
